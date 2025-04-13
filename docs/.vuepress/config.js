97f4f8d394eb60164fb1386eabc815b2707d83b6
Zhuyq0719 <zhuyq0719@Zhuyq0719deMacBook-Air.local>
2025-04-13 13:28:24 +0800

    Initialize VuePress site

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  title: '我的 VuePress 站点',
+  description: '这是一个使用 VuePress 构建的文档站点',
+  base: '/my-vuepress-site/' // 根据仓库名称设置
+}
---
+++ docs/README.md
@@ -0,0 +1,3 @@
+# Hello VuePress
+
+这是我的第一个 VuePress 文档站点。
